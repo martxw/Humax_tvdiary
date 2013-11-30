37296eadd0afc72a6e6416d6408e2dfaf86636b9
Martin Wink <martin@mwink.co.uk>
2013-11-30 18:08:49 +0000

    Mid moving display logic to JS

--- webif/html/tvdiary/script.js
+++ webif/html/tvdiary/script.js
@@ -5,7 +5,14 @@
 
 var today_start = Math.floor(new Date().getTime() / 86400000.0) * 86400 + day_start;
 
+var including_live = true;
+
 $(document).ready(function() {
+
+  $().UItoTop({easingType: 'easeOutQuart'});
+  $('.backtotop').click(function() {
+    $('html, body').animate({scrollTop: 0}, 500);
+  });
 
   $('#datepicker').datepicker({
           firstDay: 1,
@@ -19,11 +26,45 @@ $(document).ready(function() {
           }
   });
 
-  $().UItoTop({easingType: 'easeOutQuart'});
-  $('.backtotop').click(function() {
-    $('html, body').animate({scrollTop: 0}, 500);
+  $('#include_live').iphoneStyle({
+    checkedLabel: 'Show',
+    uncheckedLabel: 'Hide'
+  });
+  $('#include_live').change(function() {
+    show_live($(this).is('[checked=checked]'));
   });
 
+  function show_live(state) {
+    including_live = state;
+    $( "div#watched_inner tr.live_event" ).each(function( index ) {
+      if (state) {
+        $(this).addClass("visible_event").removeClass("hidden_event");
+      } else {
+        $(this).addClass("hidden_event").removeClass("visible_event");
+      }
+    });
+    apply_altrow( $( "div#watched_inner" ) );
+  }
+
+  function apply_altrow(data) {
+    $("tr.event_row.visible_event", data).each(function(i, tr) {
+      if (i % 2) {
+        $(tr).addClass('odd').removeClass('even');
+      } else {
+        $(tr).addClass('even').removeClass('odd');
+      }
+    });
+    return data;
+  }
+  
+  function make_all_visible(data) {
+    var jqd = $(data);
+    $('tr.event_row', jqd).each(function(i, tr) {
+      $(tr).addClass('visible_event').removeClass('hidden_event');
+    });
+    return jqd;
+  }
+  
   function request_update(chosen) {
     $('#title_date').html( " - " + $.datepicker.formatDate("d MM yy", new Date(chosen * 1000)) );
 
@@ -43,7 +84,8 @@ $(document).ready(function() {
       dataType: "text",
       url: "/tvdiary/day_view.jim?start=" + chosen + "&type=R",
       success: function(data) {
-        $('#recorded_inner').html(data);
+        $('#recorded_inner').html(make_all_visible(data));
+        apply_altrow($('#recorded_inner'));
         $('#recorded_spinner').hide('slow');
       },
       error: function(_, _, e) {
@@ -60,7 +102,8 @@ $(document).ready(function() {
       dataType: "text",
       url: "/tvdiary/day_view.jim?start=" + chosen + "&type=W",
       success: function(data) {
-        $('#watched_inner').html(data);
+        $('#watched_inner').html(make_all_visible(data));
+        show_live(including_live);
         $('#watched_spinner').hide('slow');
       },
       error: function(_, _, e) {
