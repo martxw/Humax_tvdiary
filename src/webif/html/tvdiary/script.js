1b859b38160bac5ab65453108641392f51772f29
Martin Wink <martin@mwink.co.uk>
2014-01-29 05:54:08 +0000

    Moved inventory from dialog to main page

--- src/webif/html/tvdiary/script.js
+++ src/webif/html/tvdiary/script.js
@@ -51,6 +51,16 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: 0}, 500);
   });
 
+  $("#slideset1").show();
+  $("#slideset2").hide();
+
+  $("#inventory_return").click(function() {
+    $("#slideset1").show("fade");
+    $("#slideset2").hide("fade");
+    $('#inventory_inner').html("");
+    $('#inventory_spinner').show();
+  });
+  
   // Initialize the datepicker.
   $('#datepicker').datepicker({
           firstDay: 1,
@@ -117,21 +127,6 @@ $(document).ready(function() {
     } },
     close: function(e,u) {
       $('#dejavu_dialog').empty().html('<img src="/img/loading.gif" alt="loading">');
-    }
-  });
-  
-  // Prepare reusable dialogue.
-  var $inventory_dialog = $('#inventory_dialog').dialog({
-    title: "Media - Video",
-    modal: true, autoOpen: false,
-    height: 700, width: 950,
-    show: 'scale', hide: 'fade',
-    draggable: false, resizable: true,
-    buttons: { "Close" : function() {
-      $(this).dialog('close');
-    } },
-    close: function(e,u) {
-      $('#inventory_dialog').empty().html('<img src="/img/loading.gif" alt="loading">');
     }
   });
   
@@ -417,18 +412,25 @@ $(document).ready(function() {
           url: url,
           success: function(data) {
             if (data.status == "OK" ) {
-              $inventory_dialog.html(inventory_json_to_html(data));
+              $('#inventory_inner').html(inventory_json_to_html(data));
+            } else if (data.status == "EMPTY" || data.events.length == 0) {
+              $('#inventory_inner').html("<span class=\"nothing\">Nothing</span>");
             } else {
-              $inventory_dialog.html("<span class=\"nothing\">Error: " + data.status + "</span>");
+              $('#inventory_inner').html("<span class=\"nothing\">Error: " + data.status + "</span>");
             }
+            $('#inventory_spinner').hide('slow');
           },
           error: function(_, _, e) {
             log_stuff("ajax error " + e);
-            $inventory_dialog.html("<span class=\"nothing\">Sorry, unavailable due to server error</span>");
+            $('#inventory_inner').html("<span class=\"nothing\">Sorry, unavailable due to server error</span>");
+            $('#inventory_spinner').hide('slow');
           }
         });
 
-        $inventory_dialog.dialog('open');
+        //$("#slideset1").hide('slide', {direction: 'left'}, 1000);
+        //$("#slideset2").show('slide', {direction: 'left'}, 1000);
+        $("#slideset1").hide("fade");
+        $("#slideset2").show("fade");
       } else {
         alert("The media inventory is not available in this snapshot.");
       }
@@ -491,6 +493,9 @@ $(document).ready(function() {
       // Column 4. Icons for deja vu icon.
       //
       html += "<td class=\"event_flags\">";
+      if (!event.watched) {
+        html += "<img src=\"unwatched.png\" width=16 height=16 title=\"unwatched\">";
+      }
       if (event.repeats > 0) {
         html += " <img src=\"dejavu.png\" width=16 height=16 title=\"d&eacute;j&agrave; vu?\">";
       }
